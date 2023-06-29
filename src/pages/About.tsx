6365ab9f182a0adeb6f0db3685caf0e9922059e4
Sh0bhit <sguptamds94@gmail.com>
2023-06-29 21:08:56 +0530

    added pages

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,33 @@
+import Image from "next/image";
+
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="mx-auto mt-[50px] flex w-[70%] flex-col gap-10 text-center font-firaCode">
+      <h1 className="text-[40px] font-semibold text-textBlue">About Me</h1>
+      <div>
+        <Image
+          src={"/about/about.png"}
+          alt="about"
+          width={200}
+          height={200}
+          className="float-none mx-auto ss:float-left"
+        />
+        <p className=" text-lightText">
+          My name is Shobhit Gupta, and I am a self-taught programmer who has
+          become a skilled full-stack developer. I have spent a lot of time
+          learning JavaScript, Node.js, React, Next.js, and Express since I have
+          a strong interest for coding. My love for open source projects has led
+          me to actively give back to the programming community by developing
+          useful tools and materials. My specialties include leveraging React
+          and Next.js to construct responsive and understandable user interfaces
+          as well as Node.js and Express to build scalable server-side
+          applications. I make an effort to produce high-quality work that is
+          both functional and maintainable by approaching problems from a
+          problem-solving perspective, paying close attention to detail, and
+          committing to ongoing learning. I am a useful addition to the
+          programming community because I value collaboration and good teamwork.
+        </p>
+      </div>
+    </div>
+  );
 }
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,3 +1,28 @@
+import Image from "next/image";
+
 export default function Main() {
-  return <div>Helllll</div>;
+  return (
+    <div className="mx-auto mt-[50px] flex w-[70%] flex-col gap-10 text-center font-firaCode">
+      <h1 className="text-[40px] font-semibold text-textBlue">
+        Welcome To My portfolio Website
+      </h1>
+      <p className="text-lightText">
+        Welcome to my personal programming portfolio! I'm excited to share my
+        experience, skills and projects with you. Here you'll find a selection
+        of my work in various programming languages and frameworks, showcasing
+        my abilities as a software developer. Feel free to browse through my
+        portfolio and if you have any questions or opportunities, please don't
+        hesitate to contact me. Thank you for visiting!
+      </p>
+      <div className="flex justify-center gap-10">
+        <Image src="/home/home1.png" alt={"home1"} width={200} height={200} />
+        <Image src="/home/home2.png" alt={"home2"} width={200} height={200} />
+      </div>
+      <p className="text-commentColor">
+        /* Don't forget to follow me on GitHub, Twitter, and LinkedIn to stay
+        updated on my latest projects and developments. Let's connect and grow
+        together in the tech community. */
+      </p>
+    </div>
+  );
 }
